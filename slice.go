cbb616cfd1da086491b4e598fa9bb71e544a4286
dihasananda <dihasananda@gmail.com>
2024-05-15 14:39:17 +0700

    slice lanjutan

--- slice.go
+++ slice.go
@@ -16,4 +16,41 @@ func main() {
 
 	slice4 := names[:]
 	fmt.Println(slice4)
+
+	days := [...]string{"senin", "selasa", "rabu", "kamis", "jumat", "sabtu", "minggu"}
+
+	daySlice1 := days[5:]
+	fmt.Println(daySlice1)
+
+	daySlice1[0] = "sabtu baru"
+	daySlice1[1] = "minggu baru"
+	fmt.Println(daySlice1)
+	fmt.Println(days)
+
+	daySlice2 := append(daySlice1, "libur baru")
+	daySlice2[0] = "sabtu lama"
+	fmt.Println(daySlice1)
+	fmt.Println(daySlice2)
+	fmt.Println(days)
+
+	newSlice := make([]string, 2, 5)
+	newSlice[0] = "dihas"
+	newSlice[1] = "ananda"
+	// newSlice[2] = "error" //kalau ingin menambah menggunakan append
+
+	fmt.Println(newSlice, len(newSlice), cap(newSlice))
+
+	newSlice2 := append(newSlice, "tes")
+	fmt.Println(newSlice2)
+	fmt.Println(newSlice)
+
+	fromSlice := days[:]
+	toSlice := make([]string, len(fromSlice), cap(fromSlice))
+	copy(toSlice, fromSlice)
+	fmt.Println(toSlice, fromSlice)
+
+	iniArray := [...]int{1, 2, 3, 4, 5}
+	iniSlice := []int{1, 2, 3, 4, 5}
+
+	fmt.Println(iniArray, iniSlice)
 }
